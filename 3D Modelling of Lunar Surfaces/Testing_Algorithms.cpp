61055e95fa37c7516708d48ac6d963fb5a42935b
Gabriel Cobos Tello <s186468@win.dtu.dk>
2020-10-29 13:55:30 +0100

    FAST Function Descriptor start

--- 3D Modelling of Lunar Surfaces/Testing_Algorithms.cpp
+++ 3D Modelling of Lunar Surfaces/Testing_Algorithms.cpp
@@ -9,25 +9,45 @@ using namespace cv;
 using namespace std;
 using namespace samples;
 
+vector<KeyPoint> DetectKeypointsFAST(Mat img) {
+    
+    cout << "Image size :" << img.rows << " " << img.cols << "\n";
+    vector<KeyPoint> keypointsD;
+    Ptr<FastFeatureDetector> detector = FastFeatureDetector::create();
+
+    detector->detect(img, keypointsD, Mat());
+    KeyPointsFilter::retainBest(keypointsD, 2500);
+    return keypointsD;
+}
 
 int main()
 {
-	string image_path = "C:/Users/gabri/OneDrive/University/Masters/Autumn_2020/Image Analysis with Microcomputer/Special Assigment/Test photos/IMG_4706_a.jpg";
-	Mat img = imread(image_path, IMREAD_GRAYSCALE);
-    //Mat imgS;
-    //resize(img, imgS, Size(1000, 500));
+    string  image_path1 = "C:/Users/gabri/OneDrive/University/Masters/Autumn_2020/Image Analysis with Microcomputer/Special Assigment/Test photos/IMG_4706.jpg";
+    Mat img1 = imread(image_path1, IMREAD_GRAYSCALE);
+    string  image_path2 = "C:/Users/gabri/OneDrive/University/Masters/Autumn_2020/Image Analysis with Microcomputer/Special Assigment/Test photos/IMG_4707.jpg";
+    Mat img2 = imread(image_path2, IMREAD_GRAYSCALE);
     
-    if (img.empty())
+    if (img1.empty())
     {
-        cout << "Could not read the image: " << image_path << endl;
+        cout << "Could not read the image: " << image_path1 << endl;
         cin.get(); //wait for any key press
         return -1;
     }
-    String windowName = "Lunar Surface"; //Name of the window
+    if (img2.empty())
+    {
+        cout << "Could not read the image: " << image_path2 << endl;
+        cin.get(); //wait for any key press
+        return -1;
+    }
 
-    namedWindow(windowName, WINDOW_AUTOSIZE); // Create a window
+    //String windowName = "Lunar Surface"; //Name of the window
 
-    imshow(windowName, img); // Show our image inside the created window.
+    //namedWindow(windowName, WINDOW_AUTOSIZE); // Create a window
+
+    namedWindow("luna1", WINDOW_NORMAL);
+    imshow("luna1", img1); // Show our image inside the created window.
+    namedWindow("luna2", WINDOW_NORMAL);
+    imshow("luna2", img2); // Show our image inside the created window.
 
     int k = waitKey(0); // Wait for any keystroke in the window
     //if (k == 's')
@@ -35,20 +55,22 @@ int main()
     //    imwrite("starry_night.png", img);
     //}
 
-    cout << "Image size :" << img.rows << " " << img.cols << "\n";
+    vector<KeyPoint> keypointsD1 = DetectKeypointsFAST(img1);
+    drawKeypoints(img1, keypointsD1, img1,Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    namedWindow("keypoints1", WINDOW_NORMAL);
+    imshow("keypoints1", img1);
 
-    vector<KeyPoint> keypointsD;
-    Ptr<FastFeatureDetector> detector = FastFeatureDetector::create();
-
-    
-
-    detector->detect(img, keypointsD, Mat());
-    KeyPointsFilter::retainBest(keypointsD, 2500);
-    drawKeypoints(img, keypointsD, img,Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    imshow("keypoints", img);
+    vector<KeyPoint> keypointsD2 = DetectKeypointsFAST(img2);
+    drawKeypoints(img2, keypointsD2, img2, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    namedWindow("keypoints2", WINDOW_NORMAL);
+    imshow("keypoints2", img2);
 
     k = waitKey(0);
 
-    destroyWindow(windowName); //destroy the created window
+    //Ptr<DescriptorExtractor> featureExtractor = DescriptorExtractor::create();
+
+    auto Descriptor = DescriptorExtractor::Algorithm();
+    
+    destroyAllWindows; //destroy the created window
     return 0;
 }
