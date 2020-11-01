b9b17a872430734f1b7e478258bfa904cfb90f8b
Gabriel Cobos Tello <s186468@win.dtu.dk>
2020-11-02 00:10:48 +0100

    Added functions and started ORB

--- 3D Modelling of Lunar Surfaces/Testing_Algorithms.cpp
+++ 3D Modelling of Lunar Surfaces/Testing_Algorithms.cpp
@@ -4,72 +4,135 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d.hpp>
 #include <iostream>
+#include <string>
+#include <filesystem>
+
 
 using namespace cv;
 using namespace std;
 using namespace samples;
 
-vector<KeyPoint> DetectKeypointsFAST(Mat img) {
-    
-    cout << "Image size :" << img.rows << " " << img.cols << "\n";
-    vector<KeyPoint> keypointsD;
+namespace fs = std::filesystem;
+
+vector<Mat> ImageFileGenerator(fs::path parentPath) {
+    Size size(1082, 1629);
+    vector<Mat> images;
+    Mat resize_img;
+
+    cout << "iterating through files in " << parentPath.string();
+    for (const auto& entry : fs::directory_iterator(parentPath)) {
+        auto cur_path = entry.path().string();
+
+        cout << "loading file " << cur_path << "...";
+        auto cur_image = imread(cur_path, IMREAD_GRAYSCALE);
+
+        if (cur_image.empty()) {
+            cout << "Could not read the image: " << cur_path << ", skipping image" << endl;
+            cin.get(); //wait for any key press
+            continue;
+        }
+        resize(cur_image, resize_img, size);
+
+        cout << " success! " << endl;
+        images.push_back(resize_img);
+    }
+
+    return images;
+}
+
+vector <vector<KeyPoint>> DetectKeypointsFAST(vector<Mat> imgs, int size, int threshold) {
+    vector <vector<KeyPoint>> KeypointVector;
+    KeypointVector.resize(size);
+
     Ptr<FastFeatureDetector> detector = FastFeatureDetector::create();
 
-    detector->detect(img, keypointsD, Mat());
-    KeyPointsFilter::retainBest(keypointsD, 2500);
-    return keypointsD;
+    for (int i = 0; i < size; i++) {
+        cout << "Image size :" << imgs[i].rows << " " << imgs[i].cols << "\n";
+        detector->detect(
+            imgs[i], 
+            KeypointVector[i], 
+            Mat());
+        KeyPointsFilter::retainBest(KeypointVector[i], threshold);
+    }
+    return KeypointVector;
 }
+
+//void ShowKeyPoints(vector<Mat> imgs, vector <vector<KeyPoint>> KeypointVector) {
+//    for (int i = 0; i < KeypointVector.size(); i++) {
+//        drawKeypoints(imgs[i], KeypointVector[i], imgs[i], Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+//        namedWindow(KeypointVector., WINDOW_NORMAL);
+//        imshow("keypoints1", imgs[i]);
+//    }
+//}
+
 
 int main()
 {
-    string  image_path1 = "C:/Users/gabri/OneDrive/University/Masters/Autumn_2020/Image Analysis with Microcomputer/Special Assigment/Test photos/IMG_4706.jpg";
+
+   /* string  image_path1 = "C:/Users/gabri/OneDrive/University/Masters/Autumn_2020/Image Analysis with Microcomputer/Special Assigment/Test photos/Session1/DSC_0007-2.jpg";
     Mat img1 = imread(image_path1, IMREAD_GRAYSCALE);
-    string  image_path2 = "C:/Users/gabri/OneDrive/University/Masters/Autumn_2020/Image Analysis with Microcomputer/Special Assigment/Test photos/IMG_4707.jpg";
-    Mat img2 = imread(image_path2, IMREAD_GRAYSCALE);
+    string  image_path2 = "C:/Users/gabri/OneDrive/University/Masters/Autumn_2020/Image Analysis with Microcomputer/Special Assigment/Test photos/Session1/DSC_0008-2.jpg";
+    Mat img2 = imread(image_path2, IMREAD_GRAYSCALE);*/
+
+    auto imgs = ImageFileGenerator("C:/Users/gabri/OneDrive/University/Masters/Autumn_2020/Image Analysis with Microcomputer/Special Assigment/Test photos/Session2");
     
-    if (img1.empty())
-    {
-        cout << "Could not read the image: " << image_path1 << endl;
-        cin.get(); //wait for any key press
-        return -1;
-    }
-    if (img2.empty())
-    {
-        cout << "Could not read the image: " << image_path2 << endl;
-        cin.get(); //wait for any key press
-        return -1;
-    }
+    //if (img1.empty())
+    //{
+    //    cout << "Could not read the image: " << image_path1 << endl;
+    //    cin.get(); //wait for any key press
+    //    return -1;
+    //}
+    //if (img2.empty())
+    //{
+    //    cout << "Could not read the image: " << image_path2 << endl;
+    //    cin.get(); //wait for any key press
+    //    return -1;
+    //}
 
     //String windowName = "Lunar Surface"; //Name of the window
 
     //namedWindow(windowName, WINDOW_AUTOSIZE); // Create a window
 
-    namedWindow("luna1", WINDOW_NORMAL);
-    imshow("luna1", img1); // Show our image inside the created window.
-    namedWindow("luna2", WINDOW_NORMAL);
-    imshow("luna2", img2); // Show our image inside the created window.
+    //namedWindow("luna1", WINDOW_NORMAL);
+    //imshow("luna1", imgs[1]); // Show our image inside the created window.
+    //namedWindow("luna2", WINDOW_NORMAL);
+    //imshow("luna2", imgs[2]); // Show our image inside the created window.
 
-    int k = waitKey(0); // Wait for any keystroke in the window
+    //int k = waitKey(0); // Wait for any keystroke in the window
     //if (k == 's')
     //{
     //    imwrite("starry_night.png", img);
     //}
 
-    vector<KeyPoint> keypointsD1 = DetectKeypointsFAST(img1);
-    drawKeypoints(img1, keypointsD1, img1,Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    vector <vector<KeyPoint>> keypointVector = DetectKeypointsFAST(imgs, 2, 2500);
+    
+    drawKeypoints(imgs[0], keypointVector[0], imgs[0],Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
     namedWindow("keypoints1", WINDOW_NORMAL);
-    imshow("keypoints1", img1);
+    imshow("keypoints1", imgs[0]);
 
-    vector<KeyPoint> keypointsD2 = DetectKeypointsFAST(img2);
-    drawKeypoints(img2, keypointsD2, img2, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    drawKeypoints(imgs[1], keypointVector[1], imgs[1], Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
     namedWindow("keypoints2", WINDOW_NORMAL);
-    imshow("keypoints2", img2);
+    imshow("keypoints2", imgs[1]);
 
-    k = waitKey(0);
+    int k = waitKey(0);
 
     //Ptr<DescriptorExtractor> featureExtractor = DescriptorExtractor::create();
 
-    auto Descriptor = DescriptorExtractor::Algorithm();
+    Ptr<DescriptorExtractor> Descriptor = ORB::create;
+    Mat descriptors1, descriptors2;
+    Descriptor->compute(imgs[0], keypointVector[0], descriptors1);
+    Descriptor->compute(imgs[1], keypointVector[1], descriptors2);
+
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE);
+    std::vector< DMatch > matches;
+    matcher->match(descriptors1, descriptors2, matches);
+
+    Mat img_matches;
+    drawMatches(imgs[0], keypointVector[0], imgs[1], keypointVector[1], matches, img_matches);
+
+    imshow("Matches", img_matches);
+
+    k = waitKey(0);
     
     destroyAllWindows; //destroy the created window
     return 0;
